255283cf84c1b32f1f4398185126096ceac213fb
Darya Daneshmand <79486895+daryadaneshmand@users.noreply.github.com>
2023-05-15 22:50:59 -0500

    multiple hints
    changed single hint to a set of four

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ const slots = [
 
 const tClasses = ["T1", "T2", "T3", "T4", "T5"];
 
+const hints = [
+  'From Winston-Salem North Carolina',
+  'Graduated top of her class in college',
+  'Known for her aesthetic of bright pinks and greens',
+  '“Scratch off like lotto”',
+];
+let hintIndex = 0;
+
 spinButton.addEventListener("click", () => {
   let matchingTs = false;
   let spinResults = [];
@@ -24,7 +32,8 @@ spinButton.addEventListener("click", () => {
   }
 
   if (matchingTs) {
-    hintElement.textContent = "Hint: T is known for her unique style and energetic performances.";
+    hintElement.textContent = "Hint: " + hints[hintIndex];
+    hintIndex = (hintIndex + 1) % hints.length;
   } else {
     hintElement.textContent = "";
   }
